bfe199986783e73ba9044c27f3b7bca9cbbdadb3
VSXV <sergey.vorobiev@ymail.com>
2023-09-25 17:30:44 +0300

    app finished

--- 12. grocery matching/app/app.py
+++ 12. grocery matching/app/app.py
@@ -1,50 +1,77 @@
-from fastapi import FastAPI
-import uvicorn
-from typing import Union
 import faiss
 import numpy as np
+import pandas as pd
+import uvicorn
+
+from fastapi import FastAPI
 
 app = FastAPI()
-dims = 10
+
+# Инициализация переменных
+dims = 1
 index = None
+k = 5
 
-
+# Функция для разбора строки с вектором чисел
 def parse_string(vec: str) -> list[float]:
     l = vec.split(',')
     if len(l) != dims:
         return None
     return [float(el) for el in l]
 
+# Функция, выполняемая при старте приложения
 @app.on_event("startup")
 def start():
+    # Загрузка данных и предварительная обработка
+    df_base = pd.read_csv(
+        '/Users/vs/Programming/Data Science/Data-Science-Projects/12. grocery matching/data/base.csv', 
+        index_col=0
+    )
+    
+    for column in ['6', '21', '25', '33', '44', '59', '65', '70']:
+        df_base.drop(column, axis=1, inplace=True)
+
+    df_base = df_base / np.linalg.norm(df_base, axis=1)[:, np.newaxis]
+
     global index
+    global dims
+    dims = df_base.shape[1]
     n_cells = 500
 
+    # Инициализация Faiss Index для поиска ближайших соседей
     quantizer = faiss.IndexFlatL2(dims)
     index = faiss.IndexIVFFlat(quantizer, dims, n_cells)
 
-    arr = np.ascontiguousarray(np.random.random([5000, dims]))
-    index.train(arr)
-    index.add(arr)
+    index.train(df_base)
+    index.add(df_base)
 
+# Основной маршрут, возвращающий сообщение о статусе приложения
 @app.get("/")
 def main() -> dict:
-    return{'status': 'ok', 'message': 'Hello World'}
+    return {'status': 'ok', 'message': 'Hello World'}
 
+# Маршрут для поиска ближайших соседей
 @app.get("/knn")
-def match(item: Union[str, None] = None) -> dict:
+def match(item: str):
     global index
+    global k
     if item is None:
         return {'status': 'error', 'message': 'No item provided'}
     
+    # Преобразование строки в вектор и поиск ближайших соседей
     vec = parse_string(item)
-    vec = np.ascontiguousarray(vec, dtype='float')[np.newaxis, :]
+    if vec is None:
+        return {'status': 'error', 'message': 'Invalid item format'}
 
-    knn, idx = index.search(vec, k=5)
-
-    return {'status': 'ok', 'data': [str(el) for el in idx]}
+    vec_np = np.array(vec, dtype=np.float32)
 
 
+    # Используйте vec_np для поиска ближайших соседей
+    index.nprobe = 100
+    knn, idx = index.search(vec_np.reshape(1, -1), k)
+
+    return {'status': 'ok', 'data': [str(el) for el in idx[0]]}
 
 if __name__ == "__main__":
+    # Запуск приложения с использованием Uvicorn
     uvicorn.run(app, host="localhost", port=8031)
--- 12. grocery matching/app/test.py
+++ 12. grocery matching/app/test.py
@@ -1,15 +1,35 @@
-import requests
 import numpy as np
+import pandas as pd
+import requests
 
 def main():
-    vec = np.random.rand(10)
-    s = ','.join([str(el) for el in vec])
+    # Загрузка данных из файла CSV
+    df_train = pd.read_csv(
+        "/Users/vs/Programming/Data Science/Data-Science-Projects/12. grocery matching/data/train.csv", 
+        index_col=0
+    )
+    
+    # Удаление столбца 'Target' из данных
+    df_train.drop("Target", axis=1, inplace=True)
 
-    r = requests.get('http://localhost:8031/knn', params={'item': s})
+    # Удаление указанных столбцов из данных
+    for column in ['6', '21', '25', '33', '44', '59', '65', '70']:
+        df_train.drop(column, axis=1, inplace=True)
+
+    # Нормализация данных
+    df_train = df_train / np.linalg.norm(df_train, axis=1)[:, np.newaxis]
+
+    # Преобразование данных в формат numpy array и затем в строку
+    item_data = np.ascontiguousarray(df_train.values).astype('float32')
+    item_str = ','.join([str(el) for el in item_data[0]])  # Преобразование вектора в строку
+
+    # Отправка GET-запроса к FastAPI-приложению для поиска ближайших соседей
+    r = requests.get(f'http://localhost:8031/knn?item={item_str}')
+
     if r.status_code == 200:
-        print(r.text)
+        print(r.text)  # Вывод результатов поиска
     else:
-        print('Error')
+        print('Error')  # Вывод сообщения об ошибке
 
 if __name__ == "__main__":
     main()
